7c63ee148624eebd07e8dbef12a66414871efaea
bashforger <bash.forger@gmail.com>
2021-06-11 17:27:07 +0500

    Added bird with animation

--- src/index.js
+++ src/index.js
@@ -18,38 +18,38 @@ var config = {
     scene: [Parallax]
 };
 
-var game = new Phaser.Game(config);
+let game = new Phaser.Game(config);
 
-function preload ()
-{
-    // this.load.setBaseURL('http://labs.phaser.io');
+// function preload ()
+// {
+//     // this.load.setBaseURL('http://labs.phaser.io');
 
-    this.load.image('sky', '../src/assets/images/background.png');
-    this.load.image('logo', 'http://labs.phaser.io/assets/sprites/phaser3-logo.png');
-    this.load.image('red', 'http://labs.phaser.io/assets/particles/rising-smoke.png');
-}
+//     this.load.image('sky', '../src/assets/images/background.png');
+//     this.load.image('logo', 'http://labs.phaser.io/assets/sprites/phaser3-logo.png');
+//     this.load.image('red', 'http://labs.phaser.io/assets/particles/rising-smoke.png');
+// }
 
-function create ()
-{
-    this.add.image(400, 300, 'sky');
+// function create ()
+// {
+//     this.add.image(400, 300, 'sky');
 
-    var particles = this.add.particles('red');
+//     var particles = this.add.particles('red');
 
-    var emitter = particles.createEmitter({
-        speed: 10,
-        scale: { start: 1, end: 0 },
-        blendMode: 'ADD'
-    });
+//     var emitter = particles.createEmitter({
+//         speed: 10,
+//         scale: { start: 1, end: 0 },
+//         blendMode: 'ADD'
+//     });
 
-    var logo = this.physics.add.image(400, 100, 'logo');
+//     var logo = this.physics.add.image(400, 100, 'logo');
 
-    logo.setVelocity(100, 100);
-    logo.setBounce(1, 1);
-    logo.setCollideWorldBounds(true);
+//     logo.setVelocity(100, 100);
+//     logo.setBounce(1, 1);
+//     logo.setCollideWorldBounds(true);
 
-    emitter.startFollow(logo);
-}
+//     emitter.startFollow(logo);
+// }
 
-function update ()
-{
-}
+// function update ()
+// {
+// }
